e5d8b6bc39200dab955dd2ed00068c1bc49f9233
IPBachvarov18 <IPBachvarov18@codingburgas.bg>
2020-06-26 16:03:31 +0300

    Co-Authored-By: snkolev18 <snkolev18@users.noreply.github.com> Co-Authored-By: Vsil Mladenov <VLMladenov19@users.noreply.github.com> Co-Authored-By: Kalin Chervenkov <kschervenkov19@users.noreply.github.com> Co-Authored-By:Emil Ribarev <eiribarev@users.noreply.github.com> Co-Authored-By: gngeorgiev18 <gngeorgiev18@users.noreply.github.com>

---
+++ js/trans.js
@@ -0,0 +1,40 @@
+langId = "en";
+
+var textTranslation = {
+
+    "nav-home": {
+        'en': `Home`,
+
+        'bg': `Начало`
+    }
+}
+
+var currentLanguage = 'en';
+
+function replaceElementText(item, text) {
+    if (!item.is("button")) {
+        item.html(text);
+    } else {
+        item.text(text);
+    }
+}
+
+function translateLabel(langId) {
+    currentLanguage = langId;
+
+    // Gets all tags that have 'data-lang' attribute present
+    $("[data-lang]")
+        .each(function () {
+            let item = $(this);
+            if (textTranslation.hasOwnProperty(item.data("lang")) && textTranslation[item.data("lang")].hasOwnProperty(langId)) {
+                let text = textTranslation[item.data("lang")][langId];
+                replaceElementText(item, text);
+            } else {
+                replaceElementText(item, "<font color='red'>" + item.data("lang") + "</font>");
+            }
+        })
+};
+
+function getTranslatedText(elementId) {
+    return textTranslation[elementId][currentLanguage];
+}
